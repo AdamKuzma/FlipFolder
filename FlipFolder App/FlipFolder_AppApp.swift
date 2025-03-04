8fd88ad1d735961a485c704690bee3e0c2704f85
adamkuzma <adam@esecure.cc>
2025-03-04 16:23:05 -0500

    Initial Commit

---
+++ FlipFolder App/FlipFolder_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlipFolder_AppApp.swift
+//  FlipFolder App
+//
+//  Created by Adam Kuzma on 3/4/25.
+//
+
+import SwiftUI
+
+@main
+struct FlipFolder_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FlipFolder AppTests/FlipFolder_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  FlipFolder_AppTests.swift
+//  FlipFolder AppTests
+//
+//  Created by Adam Kuzma on 3/4/25.
+//
+
+import Testing
+@testable import FlipFolder_App
+
+struct FlipFolder_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
